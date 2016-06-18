7c01347111e7611fc18194d9d3e6681a2793cefe
dsiakm <dsiak.mondala@gmail.com>
2016-06-17 22:36:29 -0300

    Working in the cinematics that preludes the battles. Working on the how to call the chat box with easy

---
+++ Assets/Scripts/Cinematics/Chatbox.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Chatbox : MonoBehaviour {
+
+	public void SetText(string dialog){
+		transform.GetChild (1).GetComponent<Text> ().text = dialog;
+	}
+
+	public void SetAvatar(string avatar){
+		transform.GetChild (0).GetComponent<RawImage>().texture = Resources.Load (avatar, typeof(Texture)) as Texture;
+	}
+
+	public GameObject SetChatbox(string avatar, string dialog){
+		GameObject go = Instantiate (Resources.Load("Chatbox", typeof(GameObject)), Vector3.zero,Quaternion.identity) as GameObject;
+		SetText (dialog);
+		SetAvatar (avatar);
+		go.transform.SetParent (GameObject.Find("Canvas").transform,false);
+
+		return go;
+
+	}
+	
+	public void DeleteChat(){
+		Destroy(transform.gameObject);
+	}
+}
---
+++ Assets/Scripts/Cinematics/CinematicChapterOne.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CinematicChapterOne : MonoBehaviour {
+
+	public GameObject Applejack;
+	GameObject chatbox;
+	Animator AJAn;
+	float time = 0.0f;
+	bool next = false;
+	int cinematic = 1;
+
+	void Awake(){
+		AJAn = Applejack.GetComponent<Animator> ();
+	}
+
+	void Update(){
+		if(cinematic == 1){
+			BuckSomeApples ();
+		}else if(cinematic == 2){
+			WalkCloser ();
+		}else if(cinematic == 3){
+			FirstDialog ();
+		}
+
+		if (Input.GetMouseButton (0) == true) {
+			next = true;
+			chatbox.GetComponent<Chatbox> ().DeleteChat ();
+		}
+
+		time += Time.deltaTime;
+	}
+		
+	void BuckSomeApples (){
+		if(time < 2){
+			return;
+		}
+		AJAn.SetBool ("buck", true);
+		if(time < 4f){
+			return;
+		}
+		AJAn.SetBool ("buck", false);
+
+		//While is a unitary function, unity freezes, do everything inside than resumes.
+		//For does not work like that.
+		int aux = 666;
+		while(aux==666){
+			GameObject.Find ("tree4").transform.GetChild (0).GetComponent<Rigidbody> ().useGravity = true;
+			GameObject.Find ("tree4").transform.GetChild (1).GetComponent<Rigidbody> ().useGravity = true;
+			GameObject.Find ("tree4").transform.GetChild (2).GetComponent<Rigidbody> ().useGravity = true;
+			GameObject.Find ("tree4").transform.GetChild (3).GetComponent<Rigidbody> ().useGravity = true;
+			GameObject.Find ("tree4").transform.GetChild (4).GetComponent<Rigidbody> ().useGravity = true;
+			aux = 0;
+		}
+		cinematic = 2;
+		time = 0;
+	}
+
+	Vector3 behindPos = new Vector3(5.83f,0.9f,-1); Vector3 closerPos = new Vector3(8.35f,-0.68f,-1); 
+	Vector3 behindScale = new Vector3(3,3,7); Vector3 closerScale = new Vector3(5,5,7); 
+	void WalkCloser(){
+		//Applejack.transform.localScale = Vector3.Lerp(behind, closer, 4f * Time.deltaTime);
+		Applejack.transform.localScale = Vector3.Lerp (behindScale, closerScale,0.4f * time);
+		Applejack.transform.position = Vector3.Lerp (behindPos, closerPos,0.4f * time);
+		AJAn.SetInteger ("side", 3);
+		AJAn.SetBool ("move", true);
+		if(Applejack.transform.position == closerPos){
+			Applejack.transform.localScale = closerScale;
+			Applejack.transform.position = closerPos;
+			AJAn.SetBool ("move", false);
+			time = 0;
+			cinematic = 3;
+		}
+	}
+
+	void FirstDialog(){
+		
+	}
+}
